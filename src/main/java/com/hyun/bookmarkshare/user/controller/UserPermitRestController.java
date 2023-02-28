5cc8c85e006db7577a16abe5366055ef5039b754
DevHyundo <08hyundo@gmail.com>
2023-02-28 13:25:35 +0900

    chore(build): Add Gson, Security, Jwt in build.gradle

--- src/main/java/com/hyun/bookmarkshare/user/controller/UserPermitRestController.java
+++ src/main/java/com/hyun/bookmarkshare/user/controller/UserPermitRestController.java
@@ -5,20 +5,27 @@ import com.hyun.bookmarkshare.user.controller.dto.LoginResponseEntity;
 import com.hyun.bookmarkshare.user.entity.User;
 import com.hyun.bookmarkshare.user.service.LoginService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
-public class LoginRestController {
+public class UserPermitRestController {
 
     private final LoginService loginService;
 
-    @PostMapping("/login/request")
-    public ResponseEntity<LoginResponseEntity> loginRequest(@RequestBody LoginRequestDto loginRequestDto){
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseEntity> loginRequest(@RequestBody @Valid LoginRequestDto loginRequestDto){
         User user = loginService.loginProcess(loginRequestDto);
         return LoginResponseEntity.toResponseEntity(user);
+    }
+
+    @DeleteMapping("/logout")
+    public ResponseEntity logout(@RequestHeader("Authorization") String token) {
+        // token repository에서 refresh Token에 해당하는 값을 삭제한다.
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 }
