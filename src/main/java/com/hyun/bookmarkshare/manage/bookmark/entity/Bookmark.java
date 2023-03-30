81186943c8e33727bcc528ff464292d547b1d456
proHyundo <guseh08@knou.ac.kr>
2023-03-30 13:21:32 +0900

    fix(swagger): Update security config to access swagger ui through custom base url

--- src/main/java/com/hyun/bookmarkshare/manage/bookmark/entity/Bookmark.java
+++ src/main/java/com/hyun/bookmarkshare/manage/bookmark/entity/Bookmark.java
@@ -1,4 +1,24 @@
 package com.hyun.bookmarkshare.manage.bookmark.entity;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
 public class Bookmark {
+
+    private Long bookmarkSeq;
+    private Long userId;
+    private Long folderSeq;
+    private String bookmarkCaption;
+    private String bookmarkScheme;
+    private String bookmarkHost;
+    private String bookmarkDomain;
+    private String bookmarkPath;
+    private String bookmarkUrl;
+    private String bookmarkDelFlag;
+
+
 }
