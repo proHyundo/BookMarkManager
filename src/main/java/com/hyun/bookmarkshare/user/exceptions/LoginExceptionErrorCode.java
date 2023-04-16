b3d4c6bad48218e79041b01de5938adc1f6b1bad
proHyundo <guseh08@knou.ac.kr>
2023-04-16 12:45:38 +0900

    feat(email): Add send verification code email

--- src/main/java/com/hyun/bookmarkshare/user/exceptions/LoginExceptionErrorCode.java
+++ src/main/java/com/hyun/bookmarkshare/user/exceptions/LoginExceptionErrorCode.java
@@ -12,12 +12,15 @@ public enum LoginExceptionErrorCode {
     BAD_INPUT_LENGTH(HttpStatus.UNAUTHORIZED, "Bad Input Length"),
     BAD_INPUT_EMAIL(HttpStatus.UNAUTHORIZED, "Bad Input Email"),
     BAD_INPUT_PWD(HttpStatus.UNAUTHORIZED, "Bad Input Pwd"),
+    INVALID_EMAIL(HttpStatus.UNAUTHORIZED, "Email is not Valid"),
 
     /* 404 NOTFOUND */
     NOT_FOUND_USER(HttpStatus.NOT_FOUND, "Wrong Id or Pwd"),
+    ALREADY_EXIST_USER(HttpStatus.UNAUTHORIZED, "Already Exist User"),
 
     /* 409 CONFLICT*/
-    INSERT_TOKEN_ERROR(HttpStatus.CONFLICT, "Bad Token");
+    INSERT_TOKEN_ERROR(HttpStatus.CONFLICT, "Bad Token"),
+    ;
 
     private final HttpStatus httpStatus;
     private final String message;
