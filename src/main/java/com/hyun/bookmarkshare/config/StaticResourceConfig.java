8004d17217f4140e169e509f6ad5909d7586b111
proHyundo <guseh08@knou.ac.kr>
2024-01-16 12:52:25 +0900

    docs(swagger): Update nginx app.conf to serving openapi3.yaml

--- src/main/java/com/hyun/bookmarkshare/config/StaticResourceConfig.java
+++ src/main/java/com/hyun/bookmarkshare/config/StaticResourceConfig.java
@@ -10,35 +10,14 @@ public class StaticResourceConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-//        registry.addResourceHandler("/swagger-ui.html")
-//                .addResourceLocations("classpath:/META-INF/resources/");
-//
-//        registry.addResourceHandler("/webjars/**")
-//                .addResourceLocations("classpath:/META-INF/resources/webjars/");
-//
-//        registry.addResourceHandler("/static/**")
-//                .addResourceLocations("classpath:/static/");
-//
-//        registry.addResourceHandler("/docs/**")
-//                .addResourceLocations("classpath:/BOOT-INF/resources/static/docs/");
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/");
+
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/static/swagger-ui/");
 
         registry.addResourceHandler("/docs/**")
                 .addResourceLocations("classpath:/static/docs/");
-//
-//        registry.addResourceHandler("/api/docs/**")
-//                .addResourceLocations("classpath:/static/docs/");
-//
-//        registry.addResourceHandler("/static/**")
-//                .addResourceLocations("classpath:/static/");
-//
-//        registry.addResourceHandler("/docs/**")
-//                .addResourceLocations("classpath:/BOOT-INF/resources/static/docs/");
-//
-//        registry.addResourceHandler("/api/docs/swagger/**")
-//                .addResourceLocations("classpath:/META-INF/resources/");
-//
-//
-//        registry.addResourceHandler("/webjars/**")
-//                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+
     }
 }
